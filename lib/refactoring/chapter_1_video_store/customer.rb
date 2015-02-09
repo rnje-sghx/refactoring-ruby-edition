da37f1ce2d31ff7627523e146f690a0afb387ce0
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-09 14:05:18 +0530

    refactor extracted Customer#amount_for from Customer#statement

--- lib/refactoring/chapter_1_video_store/customer.rb
+++ lib/refactoring/chapter_1_video_store/customer.rb
@@ -14,17 +14,7 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result                               = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = 0
-      case element.movie.price_code
-        when Movie::REGULAR
-          this_amount += 2
-          this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-        when Movie::NEW_RELEASE
-          this_amount += element.days_rented * 3
-        when Movie::CHILDRENS
-          this_amount += 1.5
-          this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-      end
+      this_amount            = amount_for(element)
       # add frequent renter points
       frequent_renter_points += 1
       # add bonus for a two day new release rental
@@ -39,5 +29,20 @@ class Customer
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
+  end
+
+  def amount_for(element)
+    this_amount = 0
+    case element.movie.price_code
+      when Movie::REGULAR
+        this_amount += 2
+        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+      when Movie::NEW_RELEASE
+        this_amount += element.days_rented * 3
+      when Movie::CHILDRENS
+        this_amount += 1.5
+        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+    end
+    this_amount
   end
 end
