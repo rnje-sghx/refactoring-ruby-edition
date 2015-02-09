a5e6ce1707afa463e16b58d54b2e28c5da8b3776
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-09 16:13:42 +0530

    refactor extracted and added Customer#charge and Customer#frequent_renter_points

--- lib/refactoring/chapter_1_video_store/movie.rb
+++ lib/refactoring/chapter_1_video_store/movie.rb
@@ -1,12 +1,31 @@
 class Refactoring::Chapter1VideoStore::Movie
   REGULAR = 0
   NEW_RELEASE = 1
-  CHILDRENS = 2
+  CHILDREN = 2
 
   attr_reader :title
   attr_accessor :price_code
 
   def initialize(title, price_code)
     @title, @price_code = title, price_code
+  end
+
+  def charge(days_rented)
+    result = 0
+    case self.price_code
+      when REGULAR
+        result += 2
+        result += (days_rented - 2) * 1.5 if days_rented > 2
+      when NEW_RELEASE
+        result += days_rented * 3
+      when CHILDREN
+        result += 1.5
+        result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
+
+  def frequent_renter_points(days_rented)
+    (self.price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
   end
 end
--- lib/refactoring/chapter_1_video_store/rental.rb
+++ lib/refactoring/chapter_1_video_store/rental.rb
@@ -6,26 +6,10 @@ class Refactoring::Chapter1VideoStore::Rental
   end
 
   def charge
-    result = 0
-    case self.movie.price_code
-      when Movie::REGULAR
-        result += 2
-        result += (self.days_rented - 2) * 1.5 if self.days_rented > 2
-      when Movie::NEW_RELEASE
-        result += self.days_rented * 3
-      when Movie::CHILDRENS
-        result += 1.5
-        result += (self.days_rented - 3) * 1.5 if self.days_rented > 3
-    end
-    result
+    self.movie.charge(self.days_rented)
   end
 
   def frequent_renter_points
-    frequent_renter_points += 1
-    # add bonus for a two day new release rental
-    if self.movie.price_code == Movie.NEW_RELEASE && self.days_rented > 1
-      frequent_renter_points += 1
-    end
-    frequent_renter_points
+    self.movie.frequent_renter_points(self.days_rented)
   end
 end
