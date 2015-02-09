1e13c653576f74b83c637083577de35d3577d2ac
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-09 17:15:58 +0530

    refactor implementation of state pattern without price_code

--- lib/refactoring/chapter_1_video_store/movie.rb
+++ lib/refactoring/chapter_1_video_store/movie.rb
@@ -1,24 +1,9 @@
 class Refactoring::Chapter1VideoStore::Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDREN = 2
+  attr_reader :title
+  attr_writer :price
 
-  attr_reader :title, :price_code
-
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
-  end
-
-  def price_code=(value)
-    @price_code = value
-    @price = case self.price_code
-               when REGULAR then
-                 RegularPrice.new
-               when NEW_RELEASE then
-                 NewReleasePrice.new
-               when CHILDREN then
-                 Children.new
-             end
+  def initialize(title, price)
+    @title, @price = title, price
   end
 
   def charge(days_rented)
