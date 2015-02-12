ccf2106576ad2e2f98bb6fdd2ea75295a0af5b79
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-12 17:14:03 +0530

    refactored Person#print_owning to Person#print_details

--- lib/refactoring/chapter_6_composing_methods/person.rb
+++ lib/refactoring/chapter_6_composing_methods/person.rb
@@ -2,9 +2,13 @@ class Refactoring::Chapter6ComposingMethods::Person
   def initialize(name)
     @name = name
   end
-  
+
   def print_owing(amount)
     print_banner
+    print_details(amount)
+  end
+
+  def print_details(amount)
     puts "name:   #{@name}"
     puts "amount: #{amount}"
   end
