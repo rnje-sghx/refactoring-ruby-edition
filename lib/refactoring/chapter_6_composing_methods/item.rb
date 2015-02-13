2cd41bfbb4bf5f2e10fe994937be39c82935f07e
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-13 16:34:59 +0530

    refactoring replace temp with query

--- lib/refactoring/chapter_6_composing_methods/item.rb
+++ lib/refactoring/chapter_6_composing_methods/item.rb
@@ -5,11 +5,14 @@ class Refactoring::Chapter6ComposingMethods::Item
   end
 
   def trade_price
-    base_price = @quantity * @price
     if (base_price > 1000)
       base_price * 0.95
     else
       base_price * 0.98
     end
+  end
+
+  def base_price
+    @quantity * @price
   end
 end
