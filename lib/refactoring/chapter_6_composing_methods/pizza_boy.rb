5584928ad51a162c536a29e5cbc1d1a91411efa2
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-13 14:41:06 +0530

    added pizza boy class for inline method refactoring

---
+++ lib/refactoring/chapter_6_composing_methods/pizza_boy.rb
@@ -0,0 +1,13 @@
+class PizzaBoy
+  def initialize()
+    @number_of_late_deliveries = 0
+  end
+
+  def get_rating
+    more_than_five_late_deliveries ? 2 : 1
+  end
+
+  def more_than_five_late_deliveries
+    @number_of_late_deliveries > 5
+  end
+end
