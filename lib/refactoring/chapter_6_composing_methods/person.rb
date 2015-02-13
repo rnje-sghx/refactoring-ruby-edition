ea3476680373af2c4664c092aa0591ce8031c128
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-13 14:04:23 +0530

    added extract method refactoring reassigning local variable

--- lib/refactoring/chapter_6_composing_methods/person.rb
+++ lib/refactoring/chapter_6_composing_methods/person.rb
@@ -4,13 +4,18 @@ class Refactoring::Chapter6ComposingMethods::Person
   end
 
   def print_owing
-    outstanding = 0.0
     print_banner
+    outstanding = calculate_outstanding
+    print_details(outstanding)
+  end
+
+  def calculate_outstanding
     # calculate outstanding
+    outstanding = 0.0
     @orders.each do |order|
       outstanding += order.amount
     end
-    print_details(outstanding)
+    outstanding
   end
 
   def print_details(outstanding)
