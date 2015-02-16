6f64a572703d3e95c0df89162d0add07dc76ddc5
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-16 14:38:36 +0530

    added initial setup for extrat surrounding method

---
+++ lib/refactoring/chapter_6_composing_methods/human.rb
@@ -0,0 +1,34 @@
+class Human
+  attr_reader :mother, :children, :name
+
+  def initialize(name, date_of_birth, date_of_death = nil, mother = nil)
+    @name          = name
+    @mother        = mother
+    @date_of_birth = date_of_birth
+    @date_of_death = date_of_death
+    @children      = []
+    @mother.add_child(self) if @mother
+  end
+
+  def add_child(child)
+    @children << child
+  end
+
+  def number_of_living_descendants
+    children.inject(0) do |count, child|
+      count += 1 if child.alive?
+      count + child.number_of_living_descendants
+    end
+  end
+
+  def number_of_descendants_named(name)
+    children.inject(0) do |count, child|
+      count += 1 if child.name == name
+      count + child.number_of_descendants_named(name)
+    end
+  end
+
+  def alive?
+    @date_of_death.nil?
+  end
+end
