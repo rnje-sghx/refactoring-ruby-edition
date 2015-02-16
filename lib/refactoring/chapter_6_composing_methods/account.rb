63664256a6c833f9fe9b31d6b9c358ce36ad659b
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-16 13:55:07 +0530

    refactoring replace method with method object

--- lib/refactoring/chapter_6_composing_methods/account.rb
+++ lib/refactoring/chapter_6_composing_methods/account.rb
@@ -1,12 +1,5 @@
 class Refactoring::Chapter6ComposingMethods::Account
   def gamma(input_val, quantity, year_to_date)
-    important_value1 = (input_val * quantity) + delta
-    important_value2 = (input_val * year_to_date) + 100
-    if (year_to_date - important_value1) > 100
-      important_value2 -= 20
-    end
-    important_value3 = important_value2 * 7
-    # and so on.
-    important_value3 - 2 * important_value1
+    Refactoring::Chapter6ComposingMethods::Gamma.new(self, input_val, quantity, year_to_date).compute
   end
 end
---
+++ lib/refactoring/chapter_6_composing_methods/gamma.rb
@@ -0,0 +1,21 @@
+class Refactoring::Chapter6ComposingMethods::Gamma
+  attr_reader :input_val, :account, :year_to_date, :quantity, :important_value1, :important_value3, :important_value2
+
+  def initialize(account, input_val, quantity, year_to_date)
+    @account      = account
+    @input_val    = input_val
+    @quantity     = quantity
+    @year_to_date = year_to_date
+  end
+
+  def compute
+    @important_value1 = (input_val * quantity) + delta
+    @important_value2 = (input_val * year_to_date) + 100
+    if (year_to_date - important_value1) > 100
+      @important_value2 -= 20
+    end
+    @important_value3 = important_value2 * 7
+    # and so on.
+    @important_value3 - 2 * important_value1
+  end
+end
