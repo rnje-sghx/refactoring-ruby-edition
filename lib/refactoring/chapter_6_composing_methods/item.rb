3bf4a7b176b774e2453234ae7ca57a6f194ef18b
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-16 13:26:30 +0530

    added Item#discount as initial setup for remove assignments to parameter

--- lib/refactoring/chapter_6_composing_methods/item.rb
+++ lib/refactoring/chapter_6_composing_methods/item.rb
@@ -27,4 +27,11 @@ class Refactoring::Chapter6ComposingMethods::Item
   def base_price
     @quantity * @price
   end
+
+  def discount(input_val, quantity, year_to_date)
+    input_val -= 2 if input_val > 50
+    input_val -= 1 if quantity > 100
+    input_val -= 4 if year_to_date > 10000
+    input_val
+  end
 end
