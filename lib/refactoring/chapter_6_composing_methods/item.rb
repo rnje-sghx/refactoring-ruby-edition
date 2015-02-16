b6010c45280f808e69b0669c72d2016709258321
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-16 13:31:43 +0530

    refactoring remove assignments to parameter

--- lib/refactoring/chapter_6_composing_methods/item.rb
+++ lib/refactoring/chapter_6_composing_methods/item.rb
@@ -29,9 +29,10 @@ class Refactoring::Chapter6ComposingMethods::Item
   end
 
   def discount(input_val, quantity, year_to_date)
-    input_val -= 2 if input_val > 50
-    input_val -= 1 if quantity > 100
-    input_val -= 4 if year_to_date > 10000
-    input_val
+    result = input_val
+    result -= 2 if input_val > 50
+    result -= 1 if quantity > 100
+    result -= 4 if year_to_date > 10000
+    result
   end
 end
