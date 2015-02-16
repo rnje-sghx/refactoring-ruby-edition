ce661c2ae0494c7a0afd9be4a38afbfd81bcb21c
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-16 12:07:54 +0530

    added introduce explaining varialble with Select#render initial setup

--- lib/refactoring/chapter_6_composing_methods/select.rb
+++ lib/refactoring/chapter_6_composing_methods/select.rb
@@ -13,4 +13,26 @@ class Select
     @options << option
     self
   end
+
+  def initialized?
+    true
+  end
+
+  def resize
+    1
+  end
+
+  def browser
+    %w(Firefox IE Chrome)
+  end
+
+  def platform
+    %w(Windows Mac Linux)
+  end
+
+  def render
+    if (platform.upcase.index('MAC') && browser.upcase.index('IE') && initialized? && resize > 0)
+      # renders
+    end
+  end
 end
