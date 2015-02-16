2f917234b60b8c936d5e02ab23b3ea172e9b84fd
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-16 14:00:58 +0530

    initial setup for substitute algorithm refactoring demonstration

---
+++ lib/refactoring/chapter_6_composing_methods/facebook.rb
@@ -0,0 +1,17 @@
+class Refactoring::Chapter6ComposingMethods::Facebook
+  def found_friends(people)
+    friends = []
+    people.each do |person|
+      if (person == 'Don')
+        friends << 'Don'
+      end
+      if (person == 'John')
+        friends << 'John'
+      end
+      if (person == 'Kent')
+        friends << 'Kent'
+      end
+    end
+    return friends
+  end
+end
