095050f6e05d08d71272d10445c4904d7dfe37c5
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-17 16:50:48 +0530

    refactoring dynacm delgation withoug method_missing and avoid SystemStackError

--- lib/refactoring/chapter_6_composing_methods/decorator.rb
+++ lib/refactoring/chapter_6_composing_methods/decorator.rb
@@ -1,9 +1,11 @@
 class Decorator
   def initialize(subject)
-    @subject = subject
-  end
-
-  def method_missing(sym, *args, &block)
-    @subject.send(sym, *args, &block)
+    subject.public_methods(false).each do |meth|
+      (class << self; self; end).class_eval do
+        define_method meth do |*args|
+          subject.send meth, *args
+        end
+      end
+    end
   end
 end
