2129fa63c315224667c4b6e1e06a3734b4583fee
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-17 17:01:40 +0530

    refactoring using user-defined data to define methods

--- lib/refactoring/chapter_6_composing_methods/person.rb
+++ lib/refactoring/chapter_6_composing_methods/person.rb
@@ -1,11 +1,12 @@
 class Person
-  attr_accessor :name, :age
-
-  def method_missing(sym, *args, &block)
-    empty?(sym.to_s.sub(/^empty_/, '').chomp('?'))
+  def self.attrs_with_empty_predicate(*args)
+    attr_accessor *args
+    args.each do |attribute|
+      define_method "empty_#{attribute}?" do
+        self.send(attribute).nil?
+      end
+    end
   end
 
-  def empty?(sym)
-    self.send(sym).nil?
-  end
+  attrs_with_empty_predicate :name, :age
 end
