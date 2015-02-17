51b8204278c11216b732cf2ed4bb28fc51336054
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-17 13:34:40 +0530

    refactoring remove unused parameter

--- lib/refactoring/chapter_6_composing_methods/product_count_item.rb
+++ lib/refactoring/chapter_6_composing_methods/product_count_item.rb
@@ -1,14 +1,9 @@
 class ProductCountItem
-  def initialize(search_criteria)
-    @search_criteria = search_criteria
-  end
-
   def self.find_all_by_criteria(criteria)
     []
   end
 
-  def product_count_items(search_criteria = nil)
-    criteria = search_criteria | @search_criteria
-    ProductCountItem.find_all_by_criteria(criteria)
+  def product_count_items(search_criteria)
+    ProductCountItem.find_all_by_criteria(search_criteria)
   end
 end
