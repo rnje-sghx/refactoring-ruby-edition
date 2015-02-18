b51c98d1c87e95a0ae4d24177efdee692e26e54f
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-18 17:34:51 +0530

    refactoring inline class

--- lib/refactoring/chapter_7_moving_features/person.rb
+++ lib/refactoring/chapter_7_moving_features/person.rb
@@ -1,12 +1,14 @@
 class Refactoring::Chapter7MovingFeatures::Person
-  attr_reader :name, :office_telephone
+  attr_reader :name
+  attr_accessor :office_area_code, :office_number
 
   def initialize(name = '', office_area_code = '', office_number = '')
     @name             = name
-    @office_telephone = TelephoneNumber.new(office_area_code, office_number)
+    @office_area_code = office_area_code
+    @office_number    = office_number
   end
 
   def telephone_number
-    @office_telephone.telephone_number
+    '(' + @office_area_code + ') ' + @office_number
   end
 end
