e27d83c560b1ea2475e8b609b98d62855a8d5181
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-18 15:35:06 +0530

    initial setup move field refactoring

--- lib/refactoring/chapter_7_moving_features/account.rb
+++ lib/refactoring/chapter_7_moving_features/account.rb
@@ -1,12 +1,17 @@
 class Refactoring::Chapter7MovingFeatures::Account
-  def initialize(account_type, days_overdrawn = 0)
+  def initialize(account_type, days_overdrawn = 0, interest_rate = 0)
     @account_type   = account_type
     @days_overdrawn = days_overdrawn
+    @interest_rate  = interest_rate
   end
 
   def bank_charge
     result = 4.5
     result += @account_type.overdraft_charge(self) if @days_overdrawn > 0
     result
+  end
+
+  def interest_for_amount_days(amount, days)
+    @interest_rate * amount * days / 365;
   end
 end
