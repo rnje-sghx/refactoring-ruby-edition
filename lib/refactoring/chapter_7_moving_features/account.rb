eeffa57f7e92f922287ea311f8f5bf550131e1f7
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-18 16:09:19 +0530

    refactoring move field self-encapsulation

--- lib/refactoring/chapter_7_moving_features/account.rb
+++ lib/refactoring/chapter_7_moving_features/account.rb
@@ -11,6 +11,10 @@ class Refactoring::Chapter7MovingFeatures::Account
   end
 
   def interest_for_amount_days(amount, days)
-    @account_type.interest_rate * amount * days / 365;
+    interest_rate * amount * days / 365;
+  end
+
+  def interest_rate
+    @account_type.interest_rate
   end
 end
