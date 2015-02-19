116ebc8a95e2903593d320f04080d44c8aacf4e6
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-19 14:14:25 +0530

    refactoring self-ecapsulation

--- lib/refactoring/chapter_7_moving_features/item.rb
+++ lib/refactoring/chapter_7_moving_features/item.rb
@@ -1,17 +1,19 @@
 module Refactoring
   module Chapter7MovingFeatures
     class Item
+      attr_accessor :base_price, :tax_rate
+
       def initialize(base_price, tax_rate)
         @base_price = base_price
         @tax_rate   = tax_rate
       end
 
       def raise_base_price_by(percent)
-        @base_price = @base_price * (1 + percent/100.0)
+        self.base_price = base_price * (1 + percent/100.0)
       end
 
       def total
-        @base_price * (1 + @tax_rate)
+        base_price * (1 + tax_rate)
       end
     end
   end
