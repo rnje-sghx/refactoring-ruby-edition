3e51fc1cfc01b59d5db59ba35ec97b434bb7d824
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-19 14:11:26 +0530

    initial setup for self encapsulation

---
+++ lib/refactoring/chapter_7_moving_features/item.rb
@@ -0,0 +1,18 @@
+module Refactoring
+  module Chapter7MovingFeatures
+    class Item
+      def initialize(base_price, tax_rate)
+        @base_price = base_price
+        @tax_rate   = tax_rate
+      end
+
+      def raise_base_price_by(percent)
+        @base_price = @base_price * (1 + percent/100.0)
+      end
+
+      def total
+        @base_price * (1 + @tax_rate)
+      end
+    end
+  end
+end
