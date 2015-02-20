e5f8bf3e1bab7e2bc855648aded0c49a146578da
Ranjeet Singh <ranjeetxsingh@gmail.com>
2015-02-20 12:54:27 +0530

    refactoring replacing constructor with factory method

--- lib/refactoring/chapter_7_moving_features/customer.rb
+++ lib/refactoring/chapter_7_moving_features/customer.rb
@@ -1,7 +1,13 @@
 module Refactoring
   module Chapter7MovingFeatures
     class Customer
+      private_class_method :new
+
       attr_reader :name
+
+      def self.create(name)
+        new(name)
+      end
 
       def initialize(name)
         @name = name
